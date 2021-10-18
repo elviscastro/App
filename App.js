bd8da21963fd9f52638ad45ab3aebf5a1d1cf127
Elvis Castro <ects@discente.ifpe.edu.br>
2021-10-18 11:31:59 -0300

    tela principal feita

--- App.js
+++ App.js
@@ -1,25 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, 
+        Button, 
+        View, 
+        SafeAreaView, 
+        Text,
+        TextInput, 
+        Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        const Separator = () => (
+          <View style={styles.separator} />
+        );
+        
+        const App = () => (
+          <SafeAreaView style={styles.container}>
+            <View>
+              <Text style={styles.title}>
+                Login:
+                <TextInput
+                  style={styles.input}
+                  placeholder="usuário"
+                />
+                <br />
+                Senha:
+                <TextInput
+                  style={styles.input}
+                  placeholder="******"
+                />
+              </Text>
+              <Button
+                title="Login"
+                onPress={() => console.log('Botao login pressionado')}
+              />
+              <Separator />
+              <Button
+                title="Cadastro"
+                onPress={() => console.log('Botao cadastro pressionado')}
+              />
+            </View>
+          </SafeAreaView>
+        );
+        
+        const styles = StyleSheet.create({
+          container: {
+            flex: 1,
+            justifyContent: 'center',
+            marginHorizontal: 16,
+          },
+          title: {
+            textAlign: 'center',
+            marginVertical: 8,
+          },
+          fixToText: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          },
+          input: {
+            height: 40,
+            margin: 12,
+            borderWidth: 1,
+            padding: 10,
+          },
+          separator: {
+            marginVertical: 8,
+            borderBottomColor: '#737373',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          },
+        });
+        
+        export default App;
