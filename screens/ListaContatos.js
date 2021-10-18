f986e08a505d6e50b5b86e2d7380b5ff77bc56e9
Elvis Castro <ects@discente.ifpe.edu.br>
2021-10-18 12:56:10 -0300

    rota de 'cadastrocontatos' criada

--- screens/ListaContatos.js
+++ screens/ListaContatos.js
@@ -1,20 +1,40 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  Text
-} from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CadastroContato from './CadastroContato'
+import { StyleSheet, Button, View, SafeAreaView, Text } from 'react-native';
 
-const ListaContatos = () => (
-  <SafeAreaView style={styles.container}>
+const Separator = () => (
+    <View style={styles.separator} />
+);
+
+function ListaContatos({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
     <View>
       <Text style={styles.title}>
         Lista de Contatos
       </Text>
+      <Button
+        title="Cadastro de Contato"
+        onPress={() => navigation.navigate('CadastroContato')}
+      />
     </View>
   </SafeAreaView>
-);
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="CadastroContato" component={CadastroContato} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -25,6 +45,15 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
     marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
